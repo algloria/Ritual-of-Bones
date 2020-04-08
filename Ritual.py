9a89a932c59f5f9b21d59d392996b0b0027b0356
Adam G <62977112+algloria@users.noreply.github.com>
2020-04-07 22:53:09 -0500

    Add files via upload

--- Ritual.py
+++ Ritual.py
@@ -1,9 +1,11 @@
 import freedice
 import Dice
 import animalcards
+import consumable
 import random
 
 damage=[1,2,3,4,5,6]
+inventory=[]
 
 print("Welcome to Ritual of Bones\n")
 print("A card game about surviving in the wilderness and pleasing the spirts of otherly realms")
@@ -22,9 +24,10 @@ elif choice=="Survivalist":
 
 
 while True:
-    choice=input("Roll the Bones? Y/N")
+    choice=input("Roll the Bones? Y/N ")
     if choice== "Y"or choice =="y":
         roll_1, roll_2 = Dice.roll()
+
         if roll_1 =="Animal Encounter" or roll_2=="Animal Encounter":
             print("You spot an animal....")
             animal = random.choice(animalcards.animal_list)
@@ -34,18 +37,33 @@ while True:
                 choice=input("Attack or Run Away? ")
                 if choice== "Attack"or choice=="attack":
                     animal.health-=attack
-                    print(f"You attack the {animal.name} for {attack} damage!")
-                    print(f"The {animal.name}'s health is now {animal.health}")
+                    animal.paces-= 1
+                    print(f"You attack the {animal.name} for {attack} damage!\n")
+                    print(f"The {animal.name}'s health is now {animal.health}\n")
                     if animal.health > 0:
                         animalattack=random.choice(animal.power)
                         Spirit-=animalattack
-                        print(f"The {animal.name} has attacked you for {animalattack}")
+                        print(f"The {animal.name} has attacked you for {animalattack}\n")
                         print(f"Your health is now {Spirit}.")
+                    if animal.health <=0:
+                    		print(f"the {animal.name} has been slayed!\n")
+                    		break
+                    if animal.paces == 0:
+                    		print(f"the {animal.name} ran away!!...\n")
+                    		break
                     if Spirit <= 0:
                         print("Game Over")
                         input()
                         exit()
-
+                elif choice=="Run away" or choice=="run away":
+                 	print(f"You manage to sneak past the {animal.name}\n")
+                 	break
+        if roll_1 == "Consumable" or roll_2 == "Consumable":
+            item=random.choice(consumable.weapon_list or consumable.consumable_list)
+            choice=input(f"Would you like to craft an {item.name} or go adventuring? Craft or Venture. ")
+            if choice=="Craft" or "craft":
+                inventory.append(item)
+                print(f"You have crafted a {item.name} and stored it in your inventory.")
                         
             
             
@@ -60,4 +78,10 @@ while True:
             print("You must either Roll the Bones or free dice")
         elif choice=="Y"or choice=="y":
             freedice.free_dice()
+    
+    elif choice =="inventory" or choice=="Inventory":
+        print(inventory)
+    
+    elif choice =="health" or choice== "health":
+        print(Spirit)
     
---
+++ consumable.py
@@ -0,0 +1,62 @@
+import random
+class Consumable:
+    def __init__(self, name, damage, spirit, frolls):
+        self.name = name
+        self.damage = damage
+        self.spirit = spirit
+        self.frolls = frolls
+
+
+consumable_list=[]
+consumable_list.append(Consumable("Beef Jerky", 0, 20, 0))
+consumable_list.append(Consumable("Camp Fire Coffee", 0, 15, 0))
+consumable_list.append(Consumable("Campfire Tea", 0, 10, 0))
+consumable_list.append(Consumable("Roasted Bear Meat", 0, 30, 0))
+consumable_list.append(Consumable("Hemp Joint", 0, 15, 0))
+consumable_list.append(Consumable("Carved Wooden Spoon", 0, 10, 0))
+consumable_list.append(Consumable("Pot of Vegetable Soup", 0, 40, 0))
+
+weapon_list=[]
+weapon_list.append(Consumable("Bow and Arrow", 20, 0, 0))
+weapon_list.append(Consumable("Iron Spear", 30, 0, 0))
+weapon_list.append(Consumable("Iron Battle Axe", 40, 0, 0))
+weapon_list.append(Consumable("Throwing Axe", 15, 0, 0))
+weapon_list.append(Consumable("Stone Knife", 15, 0, 0))
+weapon_list.append(Consumable("Stone Hatchet", 20, 0, 0))
+weapon_list.append(Consumable("Cursed Sacrifical Dagger", 50, 0, 0))
+
+treasure_list=[]
+treasure_list.append(Consumable("Golden Carrot", 0, 0, 2))
+treasure_list.append(Consumable("Iron Chest", 0, 0, 5))
+treasure_list.append(Consumable("Ancient Golden Chest", 0, 0, 8))
+treasure_list.append(Consumable("Wooden Chest", 0, 0, 3))
+treasure_list.append(Consumable("8 Leaf Clover", 0, 0, 4))
+treasure_list.append(Consumable("Sparkling Rock",0 ,0, 2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# consumable_list.append(Consumable("Deadfall Trap"))
+# consumable_list.append(Consumable("Rain Dance"))
+# consumable_list.append(Consumable("Pitfall Trap"))
+# consumable_list.append(Consumable("Loop Snare"))
+# consumable_list.append(Consumable("Animal Sacrifice"))
+# consumable_list.append(Consumable("Shaman Ritual"))
+
+
